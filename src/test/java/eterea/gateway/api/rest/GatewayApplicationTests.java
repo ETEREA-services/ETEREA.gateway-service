9439f3d22e92b7145e77e4641bd7e0469cf644e1
dqmdz <daniel.quinterospinto@gmail.com>
2024-07-09 07:43:28 -0300

    First commit

--- src/test/java/eterea/gateway/api/rest/GatewayApplicationTests.java
+++ src/test/java/eterea/gateway/api/rest/GatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.termascacheuta.eterea.gateway;
+package eterea.gateway.api.rest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayServiceApplicationTests {
+class GatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
